55e9a150d07ced918c966617a15c24803013ad83
divya999-tech <ngorrepati89@gmail.com>
2026-05-04 13:57:15 -0400

    Boolean challenge

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application");
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (yes/no)");
+            string answer = Console.ReadLine().ToLower();
+            bool dui = answer == "yes";
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified for insurance?");
+            bool qualified = (age > 15) && !dui && (tickets <= 3);
+            Console.WriteLine(qualified);
+        }
+    }
+}
