e225c387eeb49f05ecd693b581df5b5cad7d6ad0
divya999-tech <ngorrepati89@gmail.com>
2026-06-11 09:21:54 -0400

    Module 7 Assignment

---
+++ NumberLogger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace NumberLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.WriteLine("Please enter a number:");
+
+            // Read the user's input from the console
+            string userInput = Console.ReadLine();
+
+            // Define the file path where the number will be stored
+            // This creates or overwrites a file named "log.txt" in the program folder
+            string filePath = "log.txt";
+
+            // Write the user's number to the text file
+            // File.WriteAllText creates the file if it doesn't exist
+            File.WriteAllText(filePath, userInput);
+
+            // Read the contents of the text file back into the program
+            string fileContents = File.ReadAllText(filePath);
+
+            // Display the contents of the file to the user
+            Console.WriteLine("The number you entered (read from the file) is:");
+            Console.WriteLine(fileContents);
+
+            // Pause the console so the user can see the output
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumberLogger/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NumberLogger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumberLogger")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27e27dd9-cfd1-437f-a39a-c435a8b50836")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
